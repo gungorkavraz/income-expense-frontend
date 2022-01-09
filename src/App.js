4cb2c81cb4c59db3563a5314a2612165f047d480
gungorkavraz <gungor.kavraz@gmail.com>
2022-01-10 00:06:50 +0300

    Implement user auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Text } from '@chakra-ui/react';
+import routes from 'helper/routes';
+import AddCategory from 'pages/CategoryOperations/AddCategory';
+import SignIn from 'pages/UserOperations/SignIn';
+import SignUp from 'pages/UserOperations/SignUp';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from 'react-router-dom';
+
+import NavBar from '../src/pages/NavBar/NavBar.tsx';
+import { PrivateRoute } from 'pages/PrivateRoute';
+import PageNotFound from 'pages/PageNotFound';
+import { getAuthenticatedUserAsync } from 'redux/Slices/userOperationSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userOperations);
+
+  useEffect(() => {
+    // Render the component on user operations(logout/login).
+    if (user.length === 0) dispatch(getAuthenticatedUserAsync());
+  }, [user]);
+
+  console.log('rendered');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box className='App'>
+        {localStorage.getItem('accessToken') !== null ? (
+          <>
+            <NavBar></NavBar>
+            <Routes>
+              <Route
+                exact
+                path={routes.addCategory}
+                element={<PrivateRoute component={AddCategory} />}
+              />
+              <Route path={'*'} element={<PageNotFound />}></Route>
+            </Routes>
+          </>
+        ) : (
+          <>
+            <NavBar></NavBar>
+            <Routes>
+              <Route
+                exact
+                path={routes.signIn}
+                element={<PrivateRoute component={SignIn} />}
+              />
+              <Route exact path={routes.signUp} element={<SignUp />} />
+              <Route path='*' element={<SignIn />}></Route>
+            </Routes>
+          </>
+        )}
+      </Box>
+    </Router>
   );
 }
 
