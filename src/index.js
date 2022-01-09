10b441da701d0f005e53b75d1dc90b455acff1d5
gungorkavraz <gungor.kavraz@gmail.com>
2022-01-10 00:05:12 +0300

    Set to use react-redux and chakra-ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import store from 'redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
